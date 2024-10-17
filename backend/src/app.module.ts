90db756f6a4e228204bcd5032976f603200b8cb8
philippeapra <philippeaprahamian6@gmail.com>
2024-10-17 04:26:38 -0400

    backend login feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { RepositoriesModule } from '@fred/repositories/repositories.module';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { MorganModule, MorganInterceptor } from 'nest-morgan';
 import { SessionGuard } from './session/session.guard';
 import { AuthController } from './auth/auth.controller';
@@ -9,22 +10,28 @@ import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the module available globally
+    }),
     RepositoriesModule,
     MorganModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
   providers: [
-    
-      {
-          provide: APP_INTERCEPTOR,
-          useClass: MorganInterceptor('dev')
-      },
-      JwtService,
-      {
-          provide: APP_GUARD,
-          useClass: SessionGuard
-      },
-    AuthService
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('dev'),
+    },
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useClass: SessionGuard,
+    },
+    AuthService,
   ],
 })
 export class AppModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,29 +1,58 @@
 import { UserRepository } from '@fred/repositories/repositories/user.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
+import { RequestSignupDTO } from '@hubber/transfer-objects/dtos/auth';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
 
-	constructor(
-		private readonly userRepository: UserRepository,
-	) {}
-	async login(email: string, password: string) {
-		const auth = await this.userRepository.verifyPassword(email, password);
-		const { success } = auth;
-		if (!success) {
-			throw new UnauthorizedException('Invalid email or password');
-		}
+  async signup(signupDto: RequestSignupDTO) {
+    const { name, email, age, password } = signupDto;
 
-		const {user} = auth;
+    const isEmailTaken = await this.userRepository.isEmailTaken(email);
+    if (isEmailTaken) {
+      throw new UnauthorizedException('Email is already taken');
+    }
 
-		const generateAccessToken = jwt.sign({ 
-			sub: user.id,
-			email: user.email,
-			name: user.name,
-			age: user.age
-		}, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const user = await this.userRepository.create(name, email, age, password);
 
-		return generateAccessToken;
-	}
+    const accessToken = jwt.sign(
+      {
+        sub: user.id,
+        email: user.email,
+        name: user.name,
+        age: user.age,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+    );
+
+    return {
+      message: 'Signup successful',
+      accessToken,
+    };
+  }
+  async login(email: string, password: string) {
+    const auth = await this.userRepository.verifyPassword(email, password);
+    const { success } = auth;
+    if (!success) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const { user } = auth;
+
+    const generateAccessToken = jwt.sign(
+      {
+        sub: user.id,
+        email: user.email,
+        name: user.name,
+        age: user.age,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+    );
+
+    return generateAccessToken;
+  }
 }
