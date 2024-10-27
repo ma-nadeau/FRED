cb8fb15a89de51c2472510e1680b8425281c53b9
Achraf Ghellach <a.ghellach@gmail.com>
2024-10-27 16:06:53 -0400

    auth tests completed

--- backend/src/session/session.guard.spec.ts
+++ backend/src/session/session.guard.spec.ts
@@ -1,7 +1,10 @@
 import { SessionGuard } from './session.guard';
+import { ExecutionContext } from '@nestjs/common';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 describe('SessionGuard', () => {
   it('should be defined', () => {
+    const mockExecutionContext = mockDeep<ExecutionContext>();
     expect(new SessionGuard()).toBeDefined();
   });
 });
